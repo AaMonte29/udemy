c5b478e501d5383bf99d04aa8cb72c252ac01328
aaron-montenegro <aaron.d.montenegro@gmail.com>
2024-07-10 13:00:27 -0400

    MILESTONE: states
    simple component that uses reactive states along with props

--- react-introduction/src/App.jsx
+++ react-introduction/src/App.jsx
@@ -1,26 +1,9 @@
-import { Greetings } from "./Greetings";
+import { AgeCounter } from "./AgeCounter";
 
-{
-  /*Returning a component within a component as well as multiple things*/
-}
 export function App() {
   return (
     <div>
-      <h1>This is my first react project!</h1>
-      <input
-        type={"checkbox"}
-        checked
-        onChange={function () {
-          console.log("You clicked the checkbox.");
-        }}
-      />
-      <Greetings
-        firstName={"Aaron"}
-        age={22}
-        car={{ brand: "subaru", color: "white" }}
-      >
-        image={<img src="https://picsum.photos/200/300" />}
-      </Greetings>
+      <AgeCounter />
     </div>
   );
 }
