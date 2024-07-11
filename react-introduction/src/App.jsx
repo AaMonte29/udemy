2179830f1aaff55fad889ea27e3820ad91671449
aaron-montenegro <aaron.d.montenegro@gmail.com>
2024-07-11 11:34:27 -0400

    UPDATE: cleaning up files

--- react-introduction/src/App.jsx
+++ react-introduction/src/App.jsx
@@ -1,14 +1,3 @@
-import "./global.css";
-import { Car } from "./components/car/car";
-
 export function App() {
-  function sayHello(value) {
-    alert("Hello from <App/>" + value);
-  }
-  return (
-    <div>
-      <p>I am the {"<App/>"}</p>
-      <Car onCarClick={sayHello} />
-    </div>
-  );
+  return <div></div>;
 }
