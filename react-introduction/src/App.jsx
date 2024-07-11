1eda12582b655d844437803590b4e471fabcee75
aaron-montenegro <aaron.d.montenegro@gmail.com>
2024-07-11 12:22:59 -0400

    MILESTONE: updating app to show MenuList and DifficultyDisplay menus
    This is the first part of the proj

--- react-introduction/src/App.jsx
+++ react-introduction/src/App.jsx
@@ -1,9 +1,16 @@
 import { DisplayDifficulty } from "./components/DisplayDifficulty/DisplayDifficulty";
+import { MenuListItem } from "./components/MenuListItem/MenuListItem";
+import { MenuList } from "./components/MenuList/MenuList";
+import s from "./style.module.css";
 
 export function App() {
   return (
     <div>
-      <DisplayDifficulty difficulty={"easy"} />
+      <h1>Pick Your Difficulty:</h1>
+      <div className={s.pretty}>
+        <MenuList />
+        <DisplayDifficulty difficulty={"easy"} />
+      </div>
     </div>
   );
 }
